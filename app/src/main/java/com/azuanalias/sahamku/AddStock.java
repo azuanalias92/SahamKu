743acdf2be1591c2908cb97d4d6973d01332391c
Azuan Alias <azuan@fulkrum.net>
2017-12-28 15:53:27 +0800

    Design layout
    Add all layout

---
+++ app/src/main/java/com/azuanalias/sahamku/AddStock.java
@@ -0,0 +1,10 @@
+package com.azuanalias.sahamku;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Azuan on 28/12/2017.
+ */
+
+public class AddStock extends AppCompatActivity{
+}
---
+++ app/src/main/java/com/azuanalias/sahamku/Dashboard.java
@@ -0,0 +1,17 @@
+package com.azuanalias.sahamku;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Azuan on 28/12/2017.
+ */
+
+public class Dashboard extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
